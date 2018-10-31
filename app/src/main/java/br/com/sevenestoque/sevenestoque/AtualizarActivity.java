636476910ab34992d483dda08fb4794e6db1a1e0
Malu <maluizafelix@gmail.com>
2018-10-31 20:01:27 -0300

    Atividade Continua 5.

--- app/src/main/java/br/com/sevenestoque/sevenestoque/AtualizarActivity.java
+++ app/src/main/java/br/com/sevenestoque/sevenestoque/AtualizarActivity.java
@@ -2,6 +2,11 @@ package br.com.sevenestoque.sevenestoque;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,18 +15,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class AtualizarActivity extends AppCompatActivity {
+public class AtualizarActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_atualizar);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_configuracoes);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
 
         //recupera o Button e o ProgressBar do XML
         Button btnAtualizar = (Button) findViewById(R.id.btnAtualizar);
@@ -51,7 +71,6 @@ public class AtualizarActivity extends AppCompatActivity {
                 }, 10000);//milisegundos
             }
         });
-
     }
 
     @Override
@@ -66,7 +85,7 @@ public class AtualizarActivity extends AppCompatActivity {
         Intent intent;
         switch (item.getItemId()) {
             case R.id.itemTAtualizar:
-                intent = new Intent(getBaseContext(),AtualizarActivity.class);
+                intent = new Intent(getBaseContext(), AtualizarActivity.class);
                 startActivity(intent);
 //                finish();
                 return true;
@@ -86,7 +105,67 @@ public class AtualizarActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+        switch (item.getItemId()) {
+            case R.id.navHome:
+                Intent intent = new Intent(getBaseContext(), HomeActivity.class);
+                intent.putExtra("nomeBotao","Estoque");
+                startActivity(intent);
+                finish();
+                break;
 
+            case R.id.navEstoque: {
+                Intent it = new Intent(getBaseContext(), ItensHome.class);
+                it.putExtra("nomeBotao","Estoque");
+                startActivity(it);
+                finish();
+                break;
+            }
+            case R.id.navPedidoVenda: {
+                Intent it = new Intent(getBaseContext(), ItensHome.class);
+                it.putExtra("nomeBotao","Pedido de Venda");
+                startActivity(it);
+                finish();
+                break;
+            }
+            case R.id.navProdutos: {
+                Intent it = new Intent(getBaseContext(), ItensHome.class);
+                it.putExtra("nomeBotao","Produtos");
+                startActivity(it);
+                finish();
+                break;
+            }
+            case R.id.navClientes: {
+                Intent it = new Intent(getBaseContext(), ItensHome.class);
+                it.putExtra("nomeBotao","Clientes");
+                startActivity(it);
+                finish();
+                break;
+            }
+            case R.id.navSair: {
+                Intent i = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(i);
+                finish();
+                break;
+            }
+            default: {
+                Toast.makeText(this, "Menu Default", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
 }
